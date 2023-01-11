3348bbe356f1cde6cceb28a7859f4b8d5b69731a
syw1990330 <74365330+syw1990330@users.noreply.github.com>
2023-01-11 11:41:51 +0800

    close client object
    add [with client_socket ] to close client object

--- demo_client.py
+++ demo_client.py
@@ -4,9 +4,10 @@ client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 ip_address='127.0.0.1'
 port = 1234
-client_socket.connect((ip_address,port))
+with client_socket:
+  client_socket.connect((ip_address,port))
 
-client_socket.send(b'This is a test')
+  client_socket.send(b'This is a test')
 
-response = client_socket.recv(1024)
-print(response.decode())
+  response = client_socket.recv(1024)
+  print(response.decode())
